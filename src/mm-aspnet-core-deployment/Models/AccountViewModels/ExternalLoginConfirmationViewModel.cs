260b192b3492e97159eeaa1cc24c31f5396df568
Brandon Martinez <me@brandonmartinez.com>
2016-08-06 08:45:40 -0400

    Added new ASP.NET Core Project

---
+++ src/mm-aspnet-core-deployment/Models/AccountViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mm_aspnet_core_deployment.Models.AccountViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
